cc5bc76e368c7e92fa8bfac64db9993cf8e46ae8
adaraurora <adarauroraaaa@gmail.com>
2025-10-30 18:58:55 +0700

    Add Feature Read Student

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Admin\StudentController;
 
 Route::get('/', [LandingController::class, 'index'])->name('index');
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+    //untuk CRUD table Student
+    Route::resource('students', StudentController::class);
+
 });
